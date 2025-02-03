2422fdea41c49983acbdc6316fff4d688b140571
ShaztheCoder <146822467+ShaztheCoder@users.noreply.github.com>
2025-02-03 23:10:34 +0530

    Update Component.js

--- Component.js
+++ Component.js
@@ -1,1 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+], function (UIComponent) {
+    "use strict";
 
+    return UIComponent.extend("com.pdfextract.Component", {
+        metadata: {
+            manifest: "json"
+        },
+
+        init: function () {
+            // Call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // Load PDF.js library
+            sap.ui.require([
+                "com/pdfextract/libs/pdfjs/build/pdf"
+            ], function() {
+                // PDF.js is loaded
+            });
+        }
+    });
+});
